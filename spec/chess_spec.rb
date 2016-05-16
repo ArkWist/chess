7cfce3e527e08102e6ea01116d2b27a6f3c734c9
Daniel A. G. Lilley <arkwist@gmail.com>
2016-05-16 18:01:25 +0900

    Initial commit

---
+++ spec/chess_spec.rb
@@ -0,0 +1,10 @@
+# Match
+# Board --positions
+# ~Players
+# Pieces
+  # Owner
+  # Type --rules
+  # Special Data (pawn first move?)
+# Make board
+
+## check_mate check would check beginning of the turn, if there are any legal moves which allow escape from check? So if check, then checks?

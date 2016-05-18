744a4b20b3bf763e106be788cd215e3b637007b8
ArkWist <arkwist@gmail.com>
2016-05-18 09:24:46 +0900

    Write new and next_player methods before deciding to change how players work

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,1 +1,31 @@
 # Chess
+
+class Chess
+  PLAYERS = [:White, :Black]
+  attr_reader :board, :player
+
+  def initialize
+    @board = Board.new
+    @player = PLAYERS.first
+    #play
+  end
+
+  def next_player
+    @player == PLAYERS.first ? @player = PLAYERS.last : @player = PLAYERS.first
+  end
+
+  def play
+    @board.display
+    player_turn(@player)
+  end
+
+  def player_turn
+    next_player unless @board.empty?
+    move
+    @board.display
+    player_turn unless check_mate?
+  end
+
+
+# Replay should be outside of Chess class
+# So no need for reset?

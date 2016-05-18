9d141580a56aa5f8eb989be530f386679b1e4f8d
ArkWist <arkwist@gmail.com>
2016-05-18 15:57:24 +0900

    Plan classes and methods

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,31 +1,92 @@
 # Chess
 
 class Chess
-  PLAYERS = [:White, :Black]
-  attr_reader :board, :player
+  WHITE = :White
+  BLACK = :Black
+  attr_reader :player, :board
 
   def initialize
+    @player = WHITE
     @board = Board.new
-    @player = PLAYERS.first
     #play
   end
 
   def next_player
-    @player == PLAYERS.first ? @player = PLAYERS.last : @player = PLAYERS.first
+    @player == WHITE ? @player = BLACK : @player = WHITE
   end
 
   def play
     @board.display
-    player_turn(@player)
+    until check_mate?
+      next_player unless @board.empty?
+      move
+      @board.display
+    end
+    check_mate
   end
 
-  def player_turn
-    next_player unless @board.empty?
-    move
-    @board.display
-    player_turn unless check_mate?
+  def move
   end
 
+  def valid_move?()
+  end
+
+  def make_move
+  end
+
+  def check_mate?
+  end
+
+  def check_mate
+  end
+
+  #def 
+  #end
+
+end
+
+
+class Board
+
+  def initialize
+    @width, @height = 8, 8
+    @squares = Array.new(WIDTH){ Array.new(HEIGHT) }
+    @last_player = nil
+    @last_move = nil
+    set_board
+  end
+
+  def set_board
+  end
+
+  def display
+  end
+
+end
+
+
+class Piece
+end
+
+class Pawn
+end
+class Rook
+end
+class Knight
+end
+class Bishop
+end
+class Queen
+end
+class King
+end
 
 # Replay should be outside of Chess class
 # So no need for reset?
+
+
+def replay?
+end
+
+def replay
+end

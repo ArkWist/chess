7663462c2ed881031a884e13d488bbb2a2e3313b
ArkWist <arkwist@gmail.com>
2016-05-19 19:06:20 +0900

    Write test for initial piece placement

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -30,7 +30,89 @@ describe Chess do
     end
   end
   
+  describe "Board.set_board" do
+    it "puts #{Chess::WHITE} pawns in their starting positions" do
+      c.set_board(white, black)
+      expect(b.squares[0][1]).to be_instance_of(Pawn)
+      expect(b.squares[0][1].owner).to eq(white)
+      expect(b.squares[1][1]).to be_instance_of(Pawn)
+      expect(b.squares[1][1].owner).to eq(white)
+      expect(b.squares[2][1]).to be_instance_of(Pawn)
+      expect(b.squares[2][1].owner).to eq(white)
+      expect(b.squares[3][1]).to be_instance_of(Pawn)
+      expect(b.squares[3][1].owner).to eq(white)
+      expect(b.squares[4][1]).to be_instance_of(Pawn)
+      expect(b.squares[4][1].owner).to eq(white)
+      expect(b.squares[5][1]).to be_instance_of(Pawn)
+      expect(b.squares[5][1].owner).to eq(white)
+      expect(b.squares[6][1]).to be_instance_of(Pawn)
+      expect(b.squares[6][1].owner).to eq(white)
+      expect(b.squares[7][1]).to be_instance_of(Pawn)
+      expect(b.squares[7][1].owner).to eq(white)
+    end
+    it "puts #{Chess::WHITE} major pieces in their starting positions" do
+      c.set_board(white, black)
+      expect(b.squares[0][0]).to be_instance_of(Rook)
+      expect(b.squares[0][0].owner).to eq(white)
+      expect(b.squares[1][0]).to be_instance_of(Knight)
+      expect(b.squares[1][0].owner).to eq(white)
+      expect(b.squares[2][0]).to be_instance_of(Bishop)
+      expect(b.squares[2][0].owner).to eq(white)
+      expect(b.squares[3][0]).to be_instance_of(King)
+      expect(b.squares[3][0].owner).to eq(white)
+      expect(b.squares[4][0]).to be_instance_of(Queen)
+      expect(b.squares[4][0].owner).to eq(white)
+      expect(b.squares[5][0]).to be_instance_of(Bishop)
+      expect(b.squares[5][0].owner).to eq(white)
+      expect(b.squares[6][0]).to be_instance_of(Knight)
+      expect(b.squares[6][0].owner).to eq(white)
+      expect(b.squares[7][0]).to be_instance_of(Rook)
+      expect(b.squares[7][0].owner).to eq(white)
+    end
+  end
+  describe "Board.set_board" do
+    it "puts #{Chess::BLACK} pawns in their starting positions" do
+      c.set_board(white, black)
+      expect(b.squares[0][6]).to be_instance_of(Pawn)
+      expect(b.squares[0][6].owner).to eq(black)
+      expect(b.squares[1][6]).to be_instance_of(Pawn)
+      expect(b.squares[1][6].owner).to eq(black)
+      expect(b.squares[2][6]).to be_instance_of(Pawn)
+      expect(b.squares[2][6].owner).to eq(black)
+      expect(b.squares[3][6]).to be_instance_of(Pawn)
+      expect(b.squares[3][6].owner).to eq(black)
+      expect(b.squares[4][6]).to be_instance_of(Pawn)
+      expect(b.squares[4][6].owner).to eq(black)
+      expect(b.squares[5][6]).to be_instance_of(Pawn)
+      expect(b.squares[5][6].owner).to eq(black)
+      expect(b.squares[6][6]).to be_instance_of(Pawn)
+      expect(b.squares[6][6].owner).to eq(black)
+      expect(b.squares[7][6]).to be_instance_of(Pawn)
+      expect(b.squares[7][6].owner).to eq(black)
+    end
+    it "puts #{Chess::BLACK} major pieces in their starting positions" do
+      c.set_board(white, black)
+      expect(b.squares[0][7]).to be_instance_of(Rook)
+      expect(b.squares[0][7].owner).to eq(black)
+      expect(b.squares[1][7]).to be_instance_of(Knight)
+      expect(b.squares[1][7].owner).to eq(black)
+      expect(b.squares[2][7]).to be_instance_of(Bishop)
+      expect(b.squares[2][7].owner).to eq(black)
+      expect(b.squares[3][7]).to be_instance_of(King)
+      expect(b.squares[3][7].owner).to eq(black)
+      expect(b.squares[4][7]).to be_instance_of(Queen)
+      expect(b.squares[4][7].owner).to eq(black)
+      expect(b.squares[5][7]).to be_instance_of(Bishop)
+      expect(b.squares[5][7].owner).to eq(black)
+      expect(b.squares[6][7]).to be_instance_of(Knight)
+      expect(b.squares[6][7].owner).to eq(black)
+      expect(b.squares[7][7]).to be_instance_of(Rook)
+      expect(b.squares[7][7].owner).to eq(black)
+    end
+  end
   
+  
+  # Converts chess notation back and forth
   
   # Board has cells with pieces in them.
   # Easily see if piece in a spot, and who owns it, when moving.
@@ -76,7 +158,6 @@ end
 # Make board
 
 ## check_mate check would check beginning of the turn, if there are any legal moves which allow escape from check? So if check, then checks?
-
 
 
 

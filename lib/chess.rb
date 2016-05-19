cf3fa580921f3920dd6dca5cafaa82e5841319a8
ArkWist <arkwist@gmail.com>
2016-05-19 19:42:25 +0900

    Plan how Piece objects will work

--- lib/chess.rb
+++ lib/chess.rb
@@ -17,7 +17,7 @@ class Chess
 
   def play
     @board.display
-    until check_mate?
+    until check?
       next_player unless @board.empty?
       move
       @board.display
@@ -34,7 +34,7 @@ class Chess
   def make_move
   end
 
-  def check_mate?
+  def check?
   end
 
   def check_mate
@@ -47,6 +47,7 @@ end
 
 
 class Board
+  attr_reader :squares
 
   def initialize(white, black)
     @width, @height = 8, 8
@@ -61,16 +62,16 @@ class Board
     @squares[0][0] = Rook.new(white)
     @squares[1][0] = Knight.new(white)
     @squares[2][0] = Bishop.new(white)
-    @squares[3][0] = King.new(white)
-    @squares[4][0] = Queen.new(white)
+    @squares[3][0] = Queen.new(white)
+    @squares[4][0] = King.new(white)
     @squares[5][0] = Bishop.new(white)
     @squares[6][0] = Knight.new(white)
     @squares[7][0] = Rook.new(white)
     @squares.each { |col| col[6] = Pawn.new(black) }
     @squares[1][6] = Knight.new(black)
     @squares[2][6] = Bishop.new(black)
-    @squares[3][6] = King.new(black)
-    @squares[4][6] = Queen.new(black)
+    @squares[3][6] = Queen.new(black)
+    @squares[4][6] = King.new(black)
     @squares[5][6] = Bishop.new(black)
     @squares[6][6] = Knight.new(black)
     @squares[7][6] = Rook.new(black)
@@ -83,19 +84,30 @@ end
 
 
 class Piece
+  attr_accessor :moved
+  def initialize(owner)
+    @owner = owner
+    @moved = false
+  end
+  def valid_move?
+  end
+  def valid_moves
+  end
 end
 
-class Pawn
+class Pawn < Piece
+  def valid_moves
+  end
 end
-class Rook
+class Rook < Piece
 end
-class Knight
+class Knight < Piece
 end
-class Bishop
+class Bishop < Piece
 end
-class Queen
+class Queen < Piece
 end
-class King
+class King < Piece
 end
 
 # Replay should be outside of Chess class

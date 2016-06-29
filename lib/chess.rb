aa8f2823203cf44ed71907f79a7b90157864d220
Daniel A. G. Lilley <arkwist@gmail.com>
2016-06-29 17:18:40 +0900

    Transfer over working notes

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,24 @@
+=begin
+
+
+8  [r][n][b][k][q][b][n][r]
+7  [p][p][p][p][p][p][p][p]
+6  [ ][ ][ ][ ][ ][ ][ ][ ]
+5  [ ][ ][ ][ ][ ][ ][ ][ ]
+4  [ ][ ][ ][ ][ ][ ][ ][ ]
+3  [ ][ ][ ][ ][ ][ ][ ][ ]
+2  [P][P][P][P][P][P][P][P]
+1  [R][N][B][K][Q][B][N][R]
+
+    A  B  C  D  E  F  G  H
+
+@en_passant
+@moved[] << pos if rook or king
+or have unmoved, and delete when start and target from it, probably better
+
+=end
+
+
 class Chess
   PLAYERS = [:white, :black]
   

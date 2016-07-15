a3f73a0520fdc6aef60cde44fc37dcd5b3413f9b
Daniel A. G. Lilley <arkwist@gmail.com>
2016-07-15 23:44:14 +0900

    Update list of game situations to test

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -53,119 +53,12 @@ describe Chessboard do
 
 # Chessboard methods
 =begin
-initialize
-
-print_board
-
-verify_move(player, move)
-
-do_move(move)
-
 promote?(move)
-
 do_promotion(move, type)
-
-in_check?
-
-checkmate?
-
 stalemate?
-
 fifty_moves?
-
 insufficient_material?
-
 threefold_repetition?
-
-make_save_Data
-
-reset_board
-
-load_line(line)
-
-verify_move_legality(player, move)
-
-blocked_move?(player, destination)
-
-illegal_move?(origin, destination)
-
-can_en_passant?(origin, destination, <board>)
-
-is_castle_move?(origin, destination)
-
-can_castle?
-
-is_kingside?(origin, destination)
-
-get_castle_rook_position(origin, destination)
-
-under_attack?(player, pos, board)
-
-under_en_passant_attack?(player, pos, board)
-
-are_safe_moves?(player, moves, origin, board)
-
-get_enemy_captures(player, board)
-
-project_en_passant(move, board)
-
-project_castle(move, board)
-
-model_castle(origin, destination, board)
-
-clean_up_after_special_moves(move)
-
-record_double_step(move)
-
-complete_en_passant(move)
-
-complete_castle(move)
-
-make_type_list
-
-only_kings?(type_list)
-
-only_kings_and_bishops?(type_list)
-
-only_kings_and_knight?
-
-on_same_color?(type)
-
-make_white_pieces
-
-make_black_pieces
-
-make_pieces_at(player, type, *pos)
-
-make_model
-
-make_projection_model
-
-is_unmoved_at?(pos)
-
-list_to_notation(list)
-
-get_piece(pos, board)
-
-get_square(pos, board)
-
-get_piece_index(pos)
-
-remove_piece(pos)
-
-kill_piece(pos)
-
-update_tracking(move)
-
-print_rank(board, rank)
-
-print_files
-
-get_character_for(square)
-
-get_playerized_character(player, index)
-
-load_piece_data(line)
 =end
 
 end
@@ -175,79 +68,15 @@ describe Chess do
   
 # Chess methods
 =begin
-initialize
-
-start_match
-
-next_plaer
-
-play
-
-manage_match
-
-try_turn
-
-try_command(input)
-
-try_save
-
-try_load
-
-try_draw
-
-try_move(input)
-
-complete_move(move)
-
 handle_promote(move)
-
 ask_promote
-
-in_check?
-
-checkmate?
-
 stalemate?
-
 fifty_moves?
-
 insufficient_material?
-
 threefold_repetition?
-
 try_fifty_move_draw
-
 try_insufficient_material_draw
-
 try_threefold_draw
-
-ask_permission(player, &block)
-
-report_rejected_move
-
-report_rejected_slot
-
-handle_game_results(game_results)
-
-ask_file_slot(&block)
-
-get_filename(slot)
-
-slot_exists?(slot)
-
-complete_save(slot)
-
-prepare_load(slot)
-
-complete_load
-
-print_board
-
-is_raw_command?(input)
-
-is_raw_move?(input)
-
-is_raw_position?(pos)
 =end
 
 end
